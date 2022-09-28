414693ee2ce1671cd51e55be6f4891302c13693e
hdy86 <dyhong86@naver.com>
2022-09-28 18:48:59 +0900

    Coin Paprika API + input

--- src/Coins.js
+++ src/Coins.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 
-function Coins() {
+function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [pickCoin, setPickCoin] = useState(0);
+  const [myMoney, setMyMoney] = useState(0);
 
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
@@ -13,22 +15,47 @@ function Coins() {
       });
   }, []);
 
+  const onSelectChange = (event) => {
+    setPickCoin(event.target.value);
+    setMyMoney(0);
+  };
+  const onInputChange = (event) => {
+    setMyMoney(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+
   return (
     <div>
       <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
+        <select onChange={onSelectChange}>
+          <option>Select Coin!</option>
           {coins.map((coin) => (
-            <option key={coin.id}>
+            <option key={coin.id} value={coin.quotes.USD.price}>
               {coin.name} ({coin.symbol}) : {coin.quotes.USD.price} USD
             </option>
           ))}
         </select>
       )}
+      <br />
+      <br />
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onInputChange}
+          value={myMoney}
+          type="number"
+          placeholder="Write Your USD"
+        />
+        &nbsp; USD &nbsp;
+        <button>Check!</button>
+      </form>
+      <h3>You can get {myMoney / pickCoin}</h3>
     </div>
   );
 }
 
-export default Coins;
+export default App;
